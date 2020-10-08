b5d94478ff0681832177547531d4756708c66d25
Hiba Z <hiba4.z@gmail.com>
2020-10-07 20:22:14 -0400

    exercises 4+5

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,24 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import GlobalStyles from "GlobalStyles";
+
+import ArtistRoute from "../ArtistRoute";
+
+const DEFAULT_ARTIST_ID = "2CIMQHirSU0MQqyYHq0eOx";
 
 const App = () => {
   return (
     <>
-      <Router>TODO</Router>
-      <GlobalStyles />
+      <Router>
+        <ArtistRoute />
+        <Redirect to={`/artist/${DEFAULT_ARTIST_ID}`} />
+        <GlobalStyles />
+      </Router>
     </>
   );
 };
