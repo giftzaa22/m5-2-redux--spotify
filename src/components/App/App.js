9a3a35bef55e65b720fbc9285101cac72fb8c351
Hiba Z <hiba4.z@gmail.com>
2020-10-07 21:23:24 -0400

    exercise 6

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,11 +7,18 @@ import {
 } from "react-router-dom";
 import GlobalStyles from "GlobalStyles";
 
-import ArtistRoute from "../ArtistRoute";
+import ArtistRoute from "./artistRoute";
 
 const DEFAULT_ARTIST_ID = "2CIMQHirSU0MQqyYHq0eOx";
 
 const App = () => {
+  React.useEffect(() => {
+    fetch("/spotify_access_token")
+      .then((res) => res.json())
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
   return (
     <>
       <Router>
